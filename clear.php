4b884587d1f41aa91142623edcefdb9174402c7f
FB Viewer <whoiam285@gmail.com>
2021-04-18 00:21:39 +0200

    commit summary

---
+++ clear.php
@@ -0,0 +1,13 @@
+<?php
+
+if(isset($_POST['action']) and $_POST['action']=='empty'){
+$fp=fopen("dddeeasdsxxxdfflfl.txt", "w");
+fclose($fp);
+$fp=fopen("counter1.txt", "w");
+fclose($fp);
+$fp=fopen("dddeeasdsxxxdfflfle.txt", "w");
+fclose($fp);
+$fp=fopen("counter2.txt", "w");
+fclose($fp);
+}
+
